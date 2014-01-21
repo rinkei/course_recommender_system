ecc62b531fb9f143c3657d45ec2205c039db19fd
Kei Hayashi <kei.h.hayashi@gmail.com>
2014-01-21 17:13:41 +0900

    add courses#show feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ CourseRecommenderSystem::Application.routes.draw do
 
   resource :session, only: %w(new create destroy)
 
-  resources :courses, only: %w(index)
+  resources :courses, only: %w(index show)
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -2,11 +2,20 @@ require 'spec_helper'
 
 describe CoursesController do
   describe "GET index" do
-    let(:course){ FactoryGirl.create(:course) }
+    let(:course){FactoryGirl.create(:course)}
 
     it "assigns all courses as @courses" do
       get :index
       assigns(:courses).should eq([course])
+    end
+  end
+
+  describe "GET show" do
+    let(:course){FactoryGirl.create(:course)}
+
+    it "assigns the requested item as @item" do
+      get :show, {id: course.id}
+      assigns(:course).should eq(course)
     end
   end
 end
